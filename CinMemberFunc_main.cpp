5aa3f8bb3f41341ae76e5f855f18d4f00eedbebc
cbrown17 <cassandrabrown80@gmail.com>
2020-02-04 13:45:01 -0600

    Add files via upload

---
+++ CinMemberFunc_main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace  std;
+
+
+int main() {
+
+  char ch1, ch2, ch3;
+  cout << "Enter three characters: "; 
+  cin.get(ch1);
+  cin.get(ch2);
+  cin.get(ch3);
+  cout << ch1 << ch2 << ch3; 
+}
